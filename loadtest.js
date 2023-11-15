5c4a52aed46c94fb3ab7613c3760f2fafe33f37e
Spencer Wightman <spencer.wightman@gmail.com>
2023-11-15 21:24:52 +1300

    artillery setup

---
+++ loadtest.js
@@ -0,0 +1,44 @@
+const io = require('socket.io-client');
+const fetch = require('node-fetch');
+
+module.exports = {
+  setCookieAndConnectWebSocket: async function(context, events, done) {
+    try {
+      // Make an HTTP request to set the cookie
+      const response = await fetch('https://98y98340923u4.com/set-cookie', {
+        method: 'GET'
+        // Include other options if needed
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP request failed: ${response.statusText}`);
+      }
+
+      // Log cookie setting
+      console.log("Cookie set");
+
+      // Establish WebSocket connection
+      const socket = io('https://98y98340923u4.com', {
+        transports: ['websocket'],
+        withCredentials: true,
+      });
+
+      // Handle successful connection
+      socket.on('connect', () => {
+        console.log("WebSocket connection established");
+        // You can also emit events or perform actions here as needed
+      });
+
+      // Handle other WebSocket events (e.g., messages, errors)
+
+      // Keep the connection open for a specified duration
+      setTimeout(() => {
+        socket.disconnect();
+        done();
+      }, 1200000); // 20 minutes
+    } catch (error) {
+      console.error(error);
+      done();
+    }
+  }
+};
